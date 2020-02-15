f3f902f50844c2c5d5d3d257475756755fe7f143
Ican Bachors <bachor.can@gmail.com>
2020-02-15 21:33:02 +0700

    Create pc.js

---
+++ src/schema/pc.js
@@ -0,0 +1,44 @@
+const { gql, ApolloError } = require('apollo-server-express')
+const puppeteer = require('../config/puppeteer')
+const { API_BASEURL } = require('../config')
+
+const typeDef = gql`
+    type PC {
+        json: String
+    }
+    
+    extend type Query {
+        getPc(keyword: String!): [PC]
+    }
+`
+
+const resolvers = {
+    Query: {
+        // todo
+        getListMahasiswa: (_, args) => [],
+
+        getMahasiswa: async (_, args) => {
+            const browser = await puppeteer()
+            try {
+                const page = await browser.newPage()
+                await page.goto(encodeURI('http://bachors.com'))
+                
+                const result = await page.evaluate(() => {
+                    const detail = 'anu'
+                    
+                    return detail
+                })
+
+                return result
+            } catch (reason) {
+                console.log(reason)
+                return {}
+            } finally {
+                browser.close()
+            }
+        }
+    }
+}
+
+exports.typeDef = typeDef
+exports.resolvers = resolvers
