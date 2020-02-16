c6b060b18f7c423eff4bf3740a97d60f86333bb1
Ican Bachors <bachor.can@gmail.com>
2020-02-16 08:34:39 +0700

    Update pc.js

--- src/schema/pc.js
+++ src/schema/pc.js
@@ -20,7 +20,7 @@ const resolvers = {
             const browser = await puppeteer()
             try {
                 const page = await browser.newPage()
-                await page.goto(encodeURI('http://bachors.com'))
+                await page.goto(encodeURI('https://pcpartpicker.com/products/cpu/fetch/?xslug=&location=&search=&qid=1&scr=1&scr_vw=1903&scr_vh=726&scr_dw=1920&scr_dh=1080&scr_daw=1920&scr_dah=1050&scr_ddw=1903&scr_ddh=4064&ms=1580870369860'))
 				
 				await page.setRequestInterception(true);
 				  page.on("request", request => {
@@ -41,7 +41,7 @@ const resolvers = {
 					  headers: myHeaders
 					};
 
-					var myRequest = new Request("http://bachors.com", myInit);
+					var myRequest = new Request("https://pcpartpicker.com/products/cpu/fetch/?xslug=&location=&search=&qid=1&scr=1&scr_vw=1903&scr_vh=726&scr_dw=1920&scr_dh=1080&scr_daw=1920&scr_dah=1050&scr_ddw=1903&scr_ddh=4064&ms=1580870369860", myInit);
 					fetch(myRequest);
 				  });
                 
