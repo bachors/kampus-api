5a3e0a2eeeab0e3477b7b2ee2ae74a76b28bf8bb
Ican Bachors <bachor.can@gmail.com>
2020-02-16 09:08:22 +0700

    Update pc.js

--- src/schema/pc.js
+++ src/schema/pc.js
@@ -14,45 +14,46 @@ const typeDef = gql`
 
 const resolvers = {
     Query: {
-        // todo
 
-        getPc: async (_, { keyword }) => {
+        getPc: async (_, {
+            keyword
+        }) => {
             const browser = await puppeteer()
             try {
                 const page = await browser.newPage()
-                await page.goto(encodeURI("https://pcpartpicker.com/products/cpu/fetch/?xslug=&location=&search=&qid=1&scr=1&scr_vw=1903&scr_vh=726&scr_dw=1920&scr_dh=1080&scr_daw=1920&scr_dah=1050&scr_ddw=1903&scr_ddh=4064&ms=1580870369860"))
-				
-				await page.setRequestInterception(true);
-				  page.on("request", request => {
-					request.continue();
-				  });
-				  page._client.on("Network.responseReceived", data => {
-					  
-				  });
-				  await page.evaluate(() => {
-					document.cookie = "foo=bar";
-				  });
-				  await page.evaluate(() => {
-					var myHeaders = new Headers();
-					myHeaders.append("X-Custom-Header", "Hello");
+                await page.goto(encodeURI(keyword))
 
-					var myInit = {
-					  method: "GET",
-					  headers: myHeaders
-					};
+                await page.setRequestInterception(true);
+                page.on("request", request => {
+                    request.continue();
+                });
+                page._client.on("Network.responseReceived", data => {
 
-					var myRequest = new Request("https://pcpartpicker.com/products/cpu/fetch/?xslug=&location=&search=&qid=1&scr=1&scr_vw=1903&scr_vh=726&scr_dw=1920&scr_dh=1080&scr_daw=1920&scr_dah=1050&scr_ddw=1903&scr_ddh=4064&ms=1580870369860", myInit);
-					fetch(myRequest);
-				  });
-				
-				let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+                });
+                await page.evaluate(() => {
+                    document.cookie = "foo=bar";
+                });
+                await page.evaluate(({keyword}) => {
+                    var myHeaders = new Headers();
+                    myHeaders.append("X-Custom-Header", "Hello");
+
+                    var myInit = {
+                        method: "GET",
+                        headers: myHeaders
+                    };
+
+                    var myRequest = new Request(keyword, myInit);
+                    fetch(myRequest);
+                },{keyword});
+
+                let bodyHTML = await page.evaluate(() => document.body.innerHTML);
 
                 return [{
-					"json": bodyHTML
-				}]
+                    "json": bodyHTML
+                }]
 
-				  await page.waitFor(1000);
-				  
+                await page.waitFor(1000);
+
             } catch (reason) {
                 console.log(reason)
                 return {}
